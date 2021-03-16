edea22efb5e8a25c05a600277de5b4acb3b2ce88
Kevin Pham <phambamkevin@gmail.com>
2021-03-15 17:49:41 -0700

    intro folder

---
+++ practice-demo/myapp/src/main/java/com/kevinpham/springboot/demo/myapp/MyappApplication.java
@@ -0,0 +1,18 @@
+package com.kevinpham.springboot.demo.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApplication {
+
+	public static void main(String[] args) {
+		
+		// Bootstrap the Spring Boot app
+		SpringApplication.run(MyappApplication.class, args);
+		
+		
+		
+	}
+
+}
