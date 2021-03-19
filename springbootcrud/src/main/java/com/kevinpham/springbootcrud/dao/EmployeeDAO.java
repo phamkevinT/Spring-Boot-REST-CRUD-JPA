bd3b513176134340d573d1b58443b1a6c21c5a0a
Kevin Pham <phambamkevin@gmail.com>
2021-03-18 19:08:47 -0700

    CRUD Methods for DAO

--- springbootcrud/src/main/java/com/kevinpham/springbootcrud/dao/EmployeeDAO.java
+++ springbootcrud/src/main/java/com/kevinpham/springbootcrud/dao/EmployeeDAO.java
@@ -8,4 +8,13 @@ public interface EmployeeDAO {
 
 	// Return list of all employees
 	public List<Employee> findAll();
+	
+	// Find an employee by their ID
+	public Employee findById(int theId);
+	
+	// Save an employee to database
+	public void save(Employee theEmployee);
+	
+	// Save an employee by their ID
+	public void deleteById(int theId);
 }
