16f08e54870f117475c27184ae4238a4fe559788
Kevin Pham <phambamkevin@gmail.com>
2021-03-19 21:14:11 -0700

    Spring Data REST

--- spring-data-rest-crud-demo/src/main/java/com/kevinpham/springbootcrud/dao/EmployeeRepository.java
+++ spring-data-rest-crud-demo/src/main/java/com/kevinpham/springbootcrud/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.kevinpham.springbootcrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.kevinpham.springbootcrud.entity.Employee;
 
+// @RepositoryRestResource(path="members") // Alternative path otherwise default is Employees
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	
 }
