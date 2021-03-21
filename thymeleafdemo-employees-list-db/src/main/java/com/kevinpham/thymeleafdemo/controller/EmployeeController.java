cc8bea0e3d500abcdeff570cf256b49a4824fca5
Kevin Pham <phambamkevin@gmail.com>
2021-03-20 23:33:18 -0700

    Get employees from database

--- thymeleafdemo-employees-list-db/src/main/java/com/kevinpham/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo-employees-list-db/src/main/java/com/kevinpham/thymeleafdemo/controller/EmployeeController.java
@@ -1,9 +1,6 @@
 package com.kevinpham.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,35 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kevinpham.thymeleafdemo.entity.Employee;
+import com.kevinpham.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
-	// load employee data
-	private List<Employee> theEmployees;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employee
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee(1, "Emma", "Smith", "emma@luv2code.com");
-		Employee emp3 = new Employee(1, "John", "Doe", "john@luv2code.com");
-
-		
-		// create the list
-		theEmployees = new ArrayList();
-		
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	private EmployeeService employeeService;
+	
+	// Constructor injection
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	//  add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		// Get the employees from the database
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		// add to spring model
 		theModel.addAttribute("employees", theEmployees);
